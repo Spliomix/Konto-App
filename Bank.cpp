e3decbfbd61670c1d39b9dd9eba49fdc7e1443ee
Spliomix <spliomixx@gmail.com>
2018-05-22 12:49:44 +0200

    Add files via upload

--- Bank.cpp
+++ Bank.cpp
@@ -44,12 +44,12 @@ std::ostream& Bank::print(std::ostream & os) const {
 		if (konten.find(knr) == konten.end())return nullptr;
 		return (konten.find(knr)->second);
 	}
-	void Bank::kuendigen(Person& p) {
+	void Bank::kuendigen(Person& p) {}
+		p.kuendigen();
 		for(auto & elem : p.get_konten()){
 			if(konten.find(elem->get_kn())->second.use_count()==1)
 				konten.erase(elem->get_kn());
 		}
-		p.kuendigen();
 		kunden.erase(kunden.find(p.get_name()));
 	}
 
